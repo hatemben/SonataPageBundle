609a1890e76dd801de41fb13d4d7be6086bd1879
Hatem <hatem@jed.gov.sa>
2020-03-01 15:19:44 +0300

    Added new documents for ODM support

---
+++ src/Document/BaseSnapshot.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\PageBundle\Entity;
+
+use Sonata\PageBundle\Model\Snapshot;
+
+/**
+ * The class stores snapshot information.
+ *
+ * @author Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ */
+abstract class BaseSnapshot extends Snapshot
+{
+    public function prePersist()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function preUpdate()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+}
